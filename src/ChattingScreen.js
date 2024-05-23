87748eb92ad097adda7bd41acb529c2e0b513a7e
ZzeongB <>
2024-05-23 15:53:30 +0900

    live chatting implemented

--- src/ChattingScreen.js
+++ src/ChattingScreen.js
@@ -1,20 +1,58 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Chat from "./components/Chat";
 import Header from "./components/ChatroomHeader";
 import Footer from "./components/ChatroomFooter";
+import axios from "axios";
+
 
 const ChattingScreen = () => {
+  const [messages, setMessages] = useState([]);
+
+  const fetchMessages = () => {
+    axios
+      .get("http://localhost:3000/chat")
+      .then((response) => {
+        setMessages(response.data);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch chat messages");
+      });
+  };
+
+  const onChatSubmit = (message) => {
+    const date = new Date();
+    axios
+      .post("http://localhost:3000/chat", {
+        User: "me",
+        Message: message,
+        Date: date.toISOString(),
+      })
+      .then((res) => {
+        console.log("Chat message sent");
+        setMessages(prevMessages => [...prevMessages, res.data]); // Update the messages state
+  
+      })
+      .catch((Error) => {
+        console.error("Failed to send chat message", Error);
+      });
+  };
+  
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column',}}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <Header roomName={"Chatroom"} onBack={() => {}} />
-        <Chat user={"user"} style={{ flex: 1, overflow: 'auto' }}/>
-        <Footer
-          onChatSumbmit={() => {
-            console.log("Chat submitted");
-          }}
-        />
+      <Chat
+        user={"user"}
+        style={{ flex: 1, overflow: "auto" }}
+        messages={messages}
+      />
+      <Footer onChatSumbmit={onChatSubmit} />
     </div>
   );
 };
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import example from "../data/MathDNN.json";
 import { SingleChat } from "./ChatBlock";
 import styled from "styled-components";
-
+import axios from "axios";
 const Wrapper = styled.main`
   position: relative;
   top: 50px;
@@ -14,35 +14,17 @@ const Wrapper = styled.main`
   background-color: #ffffff;
 `;
 
-const Chat = ({ user }) => {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
-
-  useEffect(() => {
-    // Fetch messages from the server, TODO
-    setMessages(example);
-  }, []);
-
-  useEffect(() => {
-    console.log("Messages: ", messages);
-  }, [messages]);
-
-  const handleInputChange = (event) => {
-    setNewMessage(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    console.log("Form submitted");
-  };
-
+const Chat = ({ user, messages }) => {
   return (
     <Wrapper>
       <div className="chat-messages">
-        {messages && // Display messages
-          Object.keys(messages)
+        {messages &&
+          messages
             .slice(-20) // Display only the last 20 messages
-            .map((key) => { // key is the index of the message
-              return <SingleChat index={key} message={messages[key]} />;
+            .map((idx, message) => {
+              // key is the index of the message
+              console.log(message, idx);
+              return <SingleChat index={idx} message={messages[message]} />;
             })}
       </div>
     </Wrapper>
